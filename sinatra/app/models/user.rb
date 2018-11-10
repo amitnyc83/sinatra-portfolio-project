0578c5739178581aa224144b0875f0b68820ab11
amitnyc83 <amit_nyc@live.com>
2018-11-10 13:01:43 -0500

    create  migration for user & teams

--- sinatra/app/models/user.rb
+++ sinatra/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :teams
+  has_secure_password
 end
--- sinatra/db/migrate/20181110175227_create_teams.rb
+++ sinatra/db/migrate/20181110175227_create_teams.rb
@@ -1,6 +1,13 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.string :name
+      t.string :stadium
+      t.string :sponsor
+      t.string :home_city
+      t.string :league
+      t.integer :user_id
+
 
       t.timestamps null: false
     end
